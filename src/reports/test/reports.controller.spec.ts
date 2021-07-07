7ea9b2ae9bce6ddd0aa6cb9c67e0ed1c2be59f03
Rafael <rflernesto@gmail.com>
2021-07-06 20:52:16 -0400

    test: setup unit testing file

--- src/reports/test/reports.controller.spec.ts
+++ src/reports/test/reports.controller.spec.ts
@@ -1,3 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ReportsController } from '../reports.controller';
+import { ReportsService } from '../reports.service';
 
-describe('ReportsController testing', () => {});
+describe('ReportsController testing', () => {
+  let reportsController: ReportsController;
+  let reportsService: ReportsService;
+
+  beforeEach(() => {
+    reportsService = new ReportsService();
+    reportsController = new ReportsController(reportsService);
+  });
+
+  it('Ensure getCountryReport returns correctly', async () => {
+    const result = 'any_data';
+    jest
+      .spyOn(reportsService, 'getCountryReport')
+      .mockImplementation(() => result);
+    expect(await reportsController.getCountryReport()).toBe(result);
+  });
+});
