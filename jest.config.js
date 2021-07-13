fe631026ad27b3cab2624d74aace1dfd0687324e
Rafael <rflernesto@gmail.com>
2021-07-12 20:46:35 -0400

    config: creates jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@shelf/jest-mongodb',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['src'],
+  transform: { '.+\\.ts$': 'ts-jest' },
+  watchPathIgnorePatterns: ['globalConfig'],
 };
