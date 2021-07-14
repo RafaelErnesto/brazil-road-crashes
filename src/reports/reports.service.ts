3cfc93263e6f4ff67370eb73aea0d9d4055caa7c
Rafael <rflernesto@gmail.com>
2021-07-13 21:35:58 -0400

    feat: inject crash model to service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Crash, CrashDocument } from './schemas/crash.schema';
 
 @Injectable()
 export class ReportsService {
+
+  constructor( @InjectModel(Crash.name) private crashModel: Model<CrashDocument> ){};
+
   getCountryReport(): string {
     return 'country';
   }
