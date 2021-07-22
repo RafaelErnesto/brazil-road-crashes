551767c80da4082625373d6bea12cee93e824c89
Rafael <rflernesto@gmail.com>
2021-07-21 21:53:06 -0400

    config: add authSource to mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { ReportsModule } from './reports/reports.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 const url = process.env.MONGO_URL || 'localhost';
-
+const user = process.env.MONGO_USER;
+const pwd = process.env.MONGO_PASSWORD;
 @Module({
   imports: [
     ReportsModule,
-    MongooseModule.forRoot(`mongodb://${url}:27017/crash-api`),
+    MongooseModule.forRoot(
+      `mongodb://${user}:${pwd}@${url}:27017/crash-api?authSource=admin`,
+    ),
   ],
 })
 export class AppModule {}
